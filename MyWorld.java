ed3c2a5cd7e93c353d65d9ef93054ef60f90fa0a
glorianaz <150281309+glorianaz@users.noreply.github.com>
2023-11-22 17:23:46 -0500

    Game Over and Score Label

--- MyWorld.java
+++ MyWorld.java
@@ -7,7 +7,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    Label scoreLabel;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -15,12 +17,35 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
+        // Create the elephant object
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        // Create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
         createApple();
+    }
+    
+    /**
+     * End the game and draw 'GameOver'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
+     * Increase score 
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
     }
     
     /**
