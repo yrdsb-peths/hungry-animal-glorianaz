5e119cc9e85d271b1b9a46c1e86f5ded826ed460
glorianaz <150281309+glorianaz@users.noreply.github.com>
2023-11-30 12:08:14 -0500

    Animate Elephant Trunk

--- Elephant.java
+++ Elephant.java
@@ -9,6 +9,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    /**
+     * Constructor - The code that gets run one time when object is created
+     */
+    
+    public Elephant()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            
+        }
+        setImage(idle[0]);
+    }
+    
+    /**
+     * Animate the elephant 
+     */
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
     
     public void act()
     {
@@ -24,7 +49,11 @@ public class Elephant extends Actor
         // Remove apple if elephant eats it 
         eat();
         
+        // Animate the elephant
+        animateElephant();
+        
     }
+    
     
     /**
      * Eats the apple and spawns a new apple if an apple is eaten
