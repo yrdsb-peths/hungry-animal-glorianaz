0f7f0c4caacad3f8e060f13830130819c673422f
glorianaz <150281309+glorianaz@users.noreply.github.com>
2023-12-04 13:06:39 -0500

    Simple Timer

--- Elephant.java
+++ Elephant.java
@@ -14,7 +14,7 @@ public class Elephant extends Actor
     
     // Direction the elephant is facing
     String facing = "right";
-    
+    SimpleTimer animationTimer = new SimpleTimer();
     /**
      * Constructor - The code that gets run one time when object is created
      */
@@ -35,6 +35,8 @@ public class Elephant extends Actor
             idleLeft[i].scale(100, 100);
         }
         
+        animationTimer.mark();
+        
         // Initial elephant image
         setImage(idleRight[0]);
     }
@@ -45,6 +47,12 @@ public class Elephant extends Actor
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 120)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
