e98669ba1a759aa15ab7ebb0077a1021a72d624c
glorianaz <150281309+glorianaz@users.noreply.github.com>
2023-12-05 13:45:33 -0500

    Add game Over Sound

--- MyWorld.java
+++ MyWorld.java
@@ -20,8 +20,6 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1, false);
         
-
-        
         // Create the elephant object
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
@@ -36,10 +34,12 @@ public class MyWorld extends World
     /**
      * End the game and draw 'GameOver'
      */
+    GreenfootSound gameOverSound = new GreenfootSound("womp-womp.mp3");
     public void gameOver()
     {
         Label gameOverLabel = new Label("Game Over", 100);
         addObject(gameOverLabel, 300, 200);
+        gameOverSound.play();
     }
     
     /**
